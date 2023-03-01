00c844afd72c5c6cf459c928b661797358e402b7
aryakarthi <aryaperumal.m@gmail.com>
2023-03-01 21:20:15 +0530

    Added table of time zones

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let showBtn = document.getElementById("show-btn");
 showBtn.addEventListener('click', showTime); 
 
+let maxRows = 4, currentRows = 0;
 
 function showTime(){
+
+  if(currentRows<maxRows){
+
+  
   let getTime = document.getElementById("select-time").value+":00";
   console.log("Selected Time :"+getTime);
   
@@ -10,7 +15,7 @@ function showTime(){
   let currentDate = new Date().toJSON().slice(0, 10);
   
   let dateTime = currentDate+" "+getTime;
-  console.log("Date and Time :"+dateTime);
+  // console.log("Date and Time :"+dateTime);
   
   const dtStr = new Date(dateTime);
 
@@ -21,11 +26,28 @@ function showTime(){
     "America/Los_Angeles",
   ];
 
+
+ 
+  let tableStr = '';
+  
   TimeZones.forEach((timeZone) => {
     let strTime = dtStr.toLocaleString("en-US", { timeZone: `${timeZone}` });
-    console.log(timeZone, strTime);
-    
-  
+    // console.log(timeZone, strTime);
+
+    currentRows++;
+    tableStr += `
+        <tr>
+            <td> ${timeZone} </td>
+            <td> ${strTime} </td>
+        </tr>            
+        ` 
   });
+
+  let tableBox = document.getElementById("time-table");
+  let tabBody = document.createElement("tbody");
+  tabBody.setAttribute("id","tbody-in");
+  tabBody.innerHTML=tableStr;
+  tableBox.append(tabBody);
+}
 
 }
